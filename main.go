d162f21c5c31925008d98ec3ef5ffa02a8cd8883
rangelsen <roy.angelsen@gmail.com>
2023-01-07 22:06:25 +0100

    Cleanup

--- main.go
+++ main.go
@@ -1,61 +1,60 @@
 package main
 
 import (
-    "os"
-    "temperature-sensor/temp"
-    "time"
-    "bufio"
-    "strings"
-    "strconv"
-    "fmt"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"temperature-sensor/temp"
+	"time"
 )
 
 func main() {
 
-    temps := getTempsFromFile("temperature.txt")
+	temps := getTempsFromFile("temperature.txt")
 
-    tempReadings := make(chan temp.TemperatureReading, 1)
-    tempMeasurements := make(chan temp.TemperatureMeasurement, 1)
+	tempReadings := make(chan temp.TemperatureReading, 1)
+	tempMeasurements := make(chan temp.TemperatureMeasurement, 1)
 
-    go temp.CalcMeasurement(tempReadings, tempMeasurements, time.Second)
-    go temp.ReadTemperatures(temps, tempReadings)
+	go temp.CalcMeasurement(tempReadings, tempMeasurements, time.Second)
+	go temp.ReadTemperatures(temps, tempReadings)
 
-    for {
-        measurement := <-tempMeasurements
-        fmt.Println("measurement:", measurement)
-    }
+	for {
+		measurement := <-tempMeasurements
+		fmt.Println("measurement:", measurement)
+	}
 }
 
 func getTempsFromFile(filePath string) []float64 {
 
-    tempFile, err := os.Open(filePath)
-    defer tempFile.Close()
-  
-    if err != nil {
-        panic(err)
-    }
+	tempFile, err := os.Open(filePath)
+	defer tempFile.Close()
 
-    tempScanner := bufio.NewScanner(tempFile)
-    tempScanner.Split(bufio.ScanLines)
+	if err != nil {
+		panic(err)
+	}
 
-    return fileContentToFloat64Slice(tempScanner)
+	tempScanner := bufio.NewScanner(tempFile)
+	tempScanner.Split(bufio.ScanLines)
+
+	return fileContentToFloat64Slice(tempScanner)
 }
 
 func fileContentToFloat64Slice(tempScanner *bufio.Scanner) []float64 {
 
-    var temps []float64
+	var temps []float64
 
-    for tempScanner.Scan() {
+	for tempScanner.Scan() {
 
-        tempStr := strings.TrimSpace(tempScanner.Text())
+		tempStr := strings.TrimSpace(tempScanner.Text())
 
-        if temp, err := strconv.ParseInt(tempStr, 10, 32); err == nil {
-            temps = append(temps, float64(temp))
-        } else {
-            panic(err)
-        }
-    }
+		if temp, err := strconv.ParseInt(tempStr, 10, 32); err == nil {
+			temps = append(temps, float64(temp))
+		} else {
+			panic(err)
+		}
+	}
 
-    return temps
+	return temps
 }
-
--- temp/temp.go
+++ temp/temp.go
@@ -1,97 +1,95 @@
 package temp
 
 import (
-    "fmt"
-    "time"
-    "math"
+	"fmt"
+	"math"
+	"time"
 )
 
 type (
-    TemperatureReading struct {
-        Temperature float64
-        TimeStamp time.Time
-    }
+	TemperatureReading struct {
+		Temperature float64
+		TimeStamp   time.Time
+	}
 
-    TemperatureMeasurement struct {
-        Time MeasurementTime    `json:"time"`
-        Min float64             `json:"min"`
-        Max float64             `json:"max"`
-        Average float64         `json:"average"`
+	TemperatureMeasurement struct {
+		Time    MeasurementTime `json:"time"`
+		Min     float64         `json:"min"`
+		Max     float64         `json:"max"`
+		Average float64         `json:"average"`
+	}
 
-    }
-
-    MeasurementTime struct {
-        Start time.Time `json:"start"`
-        End time.Time   `json:"end"`
-    }
+	MeasurementTime struct {
+		Start time.Time `json:"start"`
+		End   time.Time `json:"end"`
+	}
 )
 
 func CalcMeasurement(tempReadings <-chan TemperatureReading, tempMeasurements chan<- TemperatureMeasurement, postingInterval time.Duration) {
 
-    initial := TemperatureMeasurement {
-        MeasurementTime { time.Now(), time.Now() }, math.MaxFloat64, -math.MaxFloat64, 0,
-    }
+	initial := TemperatureMeasurement{
+		MeasurementTime{time.Now(), time.Now()}, math.MaxFloat64, -math.MaxFloat64, 0,
+	}
 
-    var accumulated TemperatureMeasurement = initial
+	var accumulated TemperatureMeasurement = initial
 
-    var readingCount uint = 0
+	var readingCount uint = 0
 
-    for {
+	for {
 
-        reading := <-tempReadings
-        fmt.Println("reading: ", reading)
-        accumulated = accumulateReadings(accumulated, reading, readingCount)
-        readingCount++
+		reading := <-tempReadings
+		fmt.Println("reading: ", reading)
+		accumulated = accumulateReadings(accumulated, reading, readingCount)
+		readingCount++
 
-        if shouldPost(accumulated, postingInterval) {
+		if shouldPost(accumulated, postingInterval) {
 
-            tempMeasurements <- accumulated
-            readingCount = 0
-            accumulated = initial
-        }
-    }
+			tempMeasurements <- accumulated
+			readingCount = 0
+			accumulated = initial
+		}
+	}
 }
 
 func accumulateReadings(acc TemperatureMeasurement, reading TemperatureReading, readingCount uint) TemperatureMeasurement {
 
-    startTime := acc.Time.Start
+	startTime := acc.Time.Start
 
-    if readingCount == 0 {
-        startTime = reading.TimeStamp
-    }
+	if readingCount == 0 {
+		startTime = reading.TimeStamp
+	}
 
-    average := (acc.Average * float64(readingCount) + reading.Temperature) / float64(readingCount + 1)
+	average := (acc.Average*float64(readingCount) + reading.Temperature) / float64(readingCount+1)
 
-    return TemperatureMeasurement {
-        MeasurementTime { startTime, reading.TimeStamp },
-        math.Min(acc.Min, reading.Temperature),
-        math.Max(acc.Max, reading.Temperature),
-        average,
-    }
+	return TemperatureMeasurement{
+		MeasurementTime{startTime, reading.TimeStamp},
+		math.Min(acc.Min, reading.Temperature),
+		math.Max(acc.Max, reading.Temperature),
+		average,
+	}
 }
 
 func shouldPost(acc TemperatureMeasurement, threshold time.Duration) bool {
 
-    return acc.Time.End.Sub(acc.Time.Start) >= threshold
+	return acc.Time.End.Sub(acc.Time.Start) >= threshold
 }
 
 func ReadTemperatures(temps []float64, tempReadings chan<- TemperatureReading) {
 
-    ticker := time.NewTicker(time.Millisecond * 100)
-  
-    getTemperature := func() float64 {
-        <-ticker.C
-        temp := temps[0]
-        temps = temps[1:]
-        return temp
-    }
+	ticker := time.NewTicker(time.Millisecond * 100)
 
-    for range temps {
+	getTemperature := func() float64 {
+		<-ticker.C
+		temp := temps[0]
+		temps = temps[1:]
+		return temp
+	}
 
-        temp := getTemperature()
-        timeStamp := time.Now()
+	for range temps {
 
-        tempReadings <- TemperatureReading { temp, timeStamp }
-    }
+		temp := getTemperature()
+		timeStamp := time.Now()
+
+		tempReadings <- TemperatureReading{temp, timeStamp}
+	}
 }
-
